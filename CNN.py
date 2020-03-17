114fc5659721bc474e8c92c298c46d71e8834672
ayaanzhaque <ayaanzhaque@gmail.com>
2020-03-17 16:39:51 -0700

    CNN

---
+++ CNN.py
@@ -0,0 +1,26 @@
+import os, cv2,itertools
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+from glob import glob
+from PIL import Image
+
+# pytorch libraries
+import torch
+from torch import optim,nn
+from torch.autograd import Variable
+from torch.utils.data import DataLoader,Dataset
+from torchvision import models,transforms
+
+# sklearn libraries
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+
+# to make the results are reproducible
+np.random.seed(10)
+torch.manual_seed(10)
+torch.cuda.manual_seed(10)
+
+print(os.listdir("../input"))
