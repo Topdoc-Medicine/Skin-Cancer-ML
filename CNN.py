6212e7e704f3652c71d4e7ec8ac7d275d5e81d0f
ayaanzhaque <ayaanzhaque@gmail.com>
2020-03-18 00:10:33 -0700

    Preprocessing Complete

--- CNN.py
+++ CNN.py
@@ -11,7 +11,7 @@ import torch
 from torch import optim,nn
 from torch.autograd import Variable
 from torch.utils.data import DataLoader,Dataset
-from torchvision import models,transforms
+# from torchvision import models,transforms #error
 
 # sklearn libraries
 from sklearn.metrics import confusion_matrix
@@ -25,7 +25,7 @@ torch.cuda.manual_seed(10)
 
 print(os.listdir("./skin-cancer-mnist-ham10000"))
 
-data_dir = '../input'
+# data_dir = './skin-cancer-mnist-ham10000'
 all_image_path = glob(os.path.join(data_dir, '*', '*.jpg'))
 imageid_path_dict = {os.path.splitext(os.path.basename(x))[0]: x for x in all_image_path}
 lesion_type_dict = {
@@ -70,4 +70,83 @@ def compute_img_mean_std(image_paths):
     print("normStd = {}".format(stdevs))
     return means,stdevs
 
-norm_mean,norm_std = compute_img_mean_std(all_image_path)
+# norm_mean,norm_std = compute_img_mean_std(all_image_path)
+
+df_original = pd.read_csv(os.path.join(data_dir, 'HAM10000_metadata.csv'))
+df_original['path'] = df_original['image_id'].map(imageid_path_dict.get)
+df_original['cell_type'] = df_original['dx'].map(lesion_type_dict.get)
+df_original['cell_type_idx'] = pd.Categorical(df_original['cell_type']).codes
+df_original.head()
+
+# this will tell us how many images are associated with each lesion_id
+df_undup = df_original.groupby('lesion_id').count()
+# now we filter out lesion_id's that have only one image associated with it
+df_undup = df_undup[df_undup['image_id'] == 1]
+df_undup.reset_index(inplace=True)
+df_undup.head()
+
+# here we identify lesion_id's that have duplicate images and those that have only one image.
+def get_duplicates(x):
+    unique_list = list(df_undup['lesion_id'])
+    if x in unique_list:
+        return 'unduplicated'
+    else:
+        return 'duplicated'
+
+# create a new colum that is a copy of the lesion_id column
+df_original['duplicates'] = df_original['lesion_id']
+# apply the function to this new column
+df_original['duplicates'] = df_original['duplicates'].apply(get_duplicates)
+df_original.head()
+
+df_original['duplicates'].value_counts()
+
+# now we filter out images that don't have duplicates
+df_undup = df_original[df_original['duplicates'] == 'unduplicated']
+df_undup.shape
+
+# now we create a val set using df because we are sure that none of these images have augmented duplicates in the train set
+y = df_undup['cell_type_idx']
+_, df_val = train_test_split(df_undup, test_size=0.2, random_state=101, stratify=y)
+df_val.shape
+
+df_val['cell_type_idx'].value_counts()
+
+# This set will be df_original excluding all rows that are in the val set
+# This function identifies if an image is part of the train or val set.
+def get_val_rows(x):
+    # create a list of all the lesion_id's in the val set
+    val_list = list(df_val['image_id'])
+    if str(x) in val_list:
+        return 'val'
+    else:
+        return 'train'
+
+# identify train and val rows
+# create a new colum that is a copy of the image_id column
+df_original['train_or_val'] = df_original['image_id']
+# apply the function to this new column
+df_original['train_or_val'] = df_original['train_or_val'].apply(get_val_rows)
+# filter out train rows
+df_train = df_original[df_original['train_or_val'] == 'train']
+print(len(df_train))
+print(len(df_val))
+
+df_train['cell_type_idx'].value_counts()
+
+df_val['cell_type'].value_counts()
+
+# Copy fewer class to balance the number of 7 classes
+data_aug_rate = [15,10,5,50,0,40,5]
+for i in range(7):
+    if data_aug_rate[i]:
+        df_train=df_train.append([df_train.loc[df_train['cell_type_idx'] == i,:]]*(data_aug_rate[i]-1), ignore_index=True)
+df_train['cell_type'].value_counts()
+
+# # We can split the test set again in a validation set and a true test set:
+# df_val, df_test = train_test_split(df_val, test_size=0.5)
+df_train = df_train.reset_index()
+df_val = df_val.reset_index()
+# df_test = df_test.reset_index()
+
+#Data Analysis and Preprocessing Complete
