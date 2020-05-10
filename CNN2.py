b0834acb28312b1ffee2da9375d28368bdd48223
ayaanzhaque <ayaanzhaque@gmail.com>
2020-05-10 15:01:41 -0700

    Final CNN2

--- CNN2.py
+++ CNN2.py
@@ -192,52 +192,52 @@ print("Test: accuracy = %f  ;  loss = %f" % (accuracy, loss))
 
 model.save("weights2.h5")
 
-plot_model_history(history)
+# plot_model_history(history)
 
-# Function to plot confusion matrix
-def plot_confusion_matrix(cm, classes,
-                          normalize=False,
-                          title='Confusion matrix',
-                          cmap=plt.cm.Blues):
-    """
-    This function prints and plots the confusion matrix.
-    Normalization can be applied by setting `normalize=True`.
-    """
-    plt.imshow(cm, interpolation='nearest', cmap=cmap)
-    plt.title(title)
-    plt.colorbar()
-    tick_marks = np.arange(len(classes))
-    plt.xticks(tick_marks, classes, rotation=45)
-    plt.yticks(tick_marks, classes)
+# # Function to plot confusion matrix
+# def plot_confusion_matrix(cm, classes,
+#                           normalize=False,
+#                           title='Confusion matrix',
+#                           cmap=plt.cm.Blues):
+#     """
+#     This function prints and plots the confusion matrix.
+#     Normalization can be applied by setting `normalize=True`.
+#     """
+#     plt.imshow(cm, interpolation='nearest', cmap=cmap)
+#     plt.title(title)
+#     plt.colorbar()
+#     tick_marks = np.arange(len(classes))
+#     plt.xticks(tick_marks, classes, rotation=45)
+#     plt.yticks(tick_marks, classes)
 
-    if normalize:
-        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+#     if normalize:
+#         cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
 
-    thresh = cm.max() / 2.
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        plt.text(j, i, cm[i, j],
-                 horizontalalignment="center",
-                 color="white" if cm[i, j] > thresh else "black")
+#     thresh = cm.max() / 2.
+#     for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+#         plt.text(j, i, cm[i, j],
+#                  horizontalalignment="center",
+#                  color="white" if cm[i, j] > thresh else "black")
 
-    plt.tight_layout()
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label')
+#     plt.tight_layout()
+#     plt.ylabel('True label')
+#     plt.xlabel('Predicted label')
 
-# Predict the values from the validation dataset
-Y_pred = model.predict(x_validate)
-# Convert predictions classes to one hot vectors
-Y_pred_classes = np.argmax(Y_pred,axis = 1)
-# Convert validation observations to one hot vectors
-Y_true = np.argmax(y_validate,axis = 1)
-# compute the confusion matrix
-confusion_mtx = confusion_matrix(Y_true, Y_pred_classes)
+# # Predict the values from the validation dataset
+# Y_pred = model.predict(x_validate)
+# # Convert predictions classes to one hot vectors
+# Y_pred_classes = np.argmax(Y_pred,axis = 1)
+# # Convert validation observations to one hot vectors
+# Y_true = np.argmax(y_validate,axis = 1)
+# # compute the confusion matrix
+# confusion_mtx = confusion_matrix(Y_true, Y_pred_classes)
 
 
 
-# plot the confusion matrix
-plot_confusion_matrix(confusion_mtx, classes = range(7))
+# # plot the confusion matrix
+# plot_confusion_matrix(confusion_mtx, classes = range(7))
 
-label_frac_error = 1 - np.diag(confusion_mtx) / np.sum(confusion_mtx, axis=1)
-plt.bar(np.arange(7),label_frac_error)
-plt.xlabel('True Label')
-plt.ylabel('Fraction classified incorrectly')
+# label_frac_error = 1 - np.diag(confusion_mtx) / np.sum(confusion_mtx, axis=1)
+# plt.bar(np.arange(7),label_frac_error)
+# plt.xlabel('True Label')
+# plt.ylabel('Fraction classified incorrectly')
